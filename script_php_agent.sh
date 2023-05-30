4e7f907e27db601d9f664f5488c9b8e3a763e7de
Samantha Barros <samantha.barros@vectracs.com.br>
2023-05-30 11:12:48 -0300

    adicionando versão do PHP

--- script_php_agent.sh
+++ script_php_agent.sh
@@ -20,6 +20,6 @@ tar -xvjf appdynamics-php-agent-x64-linux-23.4.0.724.tar.bz2
 
 echo "Instalando agente..."
 
-#/opt/appdynamics/php-agent/appdynamics-php-agent-linux_x64/install.sh [-s] -a=customer1@<account_access_key> [--http-proxy-host=<proxy_host>] [--http-proxy-port=<proxy_port>] [-e <php_ext_dir>] [-i <php_ini_dir>] [-p <php_binary_dir>] [-v <php_version>] --log-dir=/opt/appdynamics/php-agent/logs <controller-host> <controller-port> drive_expresso <tier_name> LNXCLOUDWEB
+#/opt/appdynamics/php-agent/appdynamics-php-agent-linux_x64/install.sh [-s] -a=customer1@<account_access_key> [--http-proxy-host=<proxy_host>] [--http-proxy-port=<proxy_port>] [-e <php_ext_dir>] [-i <php_ini_dir>] [-p <php_binary_dir>] -v 7.0.33 --log-dir=/opt/appdynamics/php-agent/logs <controller-host> <controller-port> drive_expresso <tier_name> LNXCLOUDWEB
 
 echo "Instalação finalizada!"
